ad0d7adf42d557ec18d20cb00afd638326f7ff97
osada <sub210sub@gmail.com>
2025-03-02 23:00:39 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import json
-import requests
-import re
+# -*- coding: utf-8 -*-
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
-    ApiClient, Configuration, MessagingApi,
-    ReplyMessageRequest, PushMessageRequest,
-    TextMessage, PostbackAction
+	ApiClient, Configuration, MessagingApi,
+	ReplyMessageRequest, PushMessageRequest,
+	TextMessage, PostbackAction
 )
 from linebot.v3.webhooks import (
-    FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
 )
 import os
-
 
 ## .env ファイル読み込み
 from dotenv import load_dotenv
@@ -24,7 +19,6 @@ load_dotenv()
 ## 環境変数を変数に割り当て
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
-
 
 ## Flask アプリのインスタンス化
 app = Flask(__name__)
@@ -36,77 +30,65 @@ handler = WebhookHandler(CHANNEL_SECRET)
 ## コールバックのおまじない
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
+## 友達追加時のメッセージ送信
+@handler.add(FollowEvent)
+def handle_follow(event):
+	## APIインスタンス化
+	with ApiClient(configuration) as api_client:
+		line_bot_api = MessagingApi(api_client)
 
-
+	## 返信
+	line_bot_api.reply_message(ReplyMessageRequest(
+		replyToken=event.reply_token,
+		messages=[TextMessage(text='Thank You!')]
+	))
+	
+## オウム返しメッセージ
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    ## APIインスタンス化
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
+	## APIインスタンス化
+	with ApiClient(configuration) as api_client:
+		line_bot_api = MessagingApi(api_client)
 
-    ## 受信メッセージの中身を取得
-    received_message = event.message.text
+	## 受信メッセージの中身を取得
+	received_message = event.message.text
 
-    if received_message == "@anime":
-    ## テキスト返信
+	## APIを呼んで送信者のプロフィール取得
+	profile = line_bot_api.get_profile(event.source.user_id)
+	display_name = profile.display_name
 
-        #HTML情報の取得・解析
-        res = requests.get('https://anime.eiga.com/program/')
-        soup = BeautifulSoup(res.text, 'html.parser')
+	## 返信メッセージ編集
+	reply = f'{display_name}さんのメッセージ\n{received_message}'
 
-        # 必要なデータのみ抽出
-        animeTtl = (soup.find_all(class_ = "seasonAnimeTtl"))
-        animeImg = (soup.find_all("img"))
-        anime_data = (soup.find_all(class_ = "seasonAnimeDetail"))
+	## オウム返し
+	line_bot_api.reply_message(ReplyMessageRequest(
+		replyToken=event.reply_token,
+		messages=[TextMessage(text=reply)]
+	))
 
-
-        # 重複している要素を削除
-        def dedup_and_restore(data):
-            reversed = data[::-1]
-            unique_reversed = sorted(set(reversed), key=reversed.index)
-            return unique_reversed[::-1]
-
-        anime_Ttl = dedup_and_restore(animeTtl)
-        anime_Img = dedup_and_restore(animeImg)
-        anime_Img = [img.get("src") for img in anime_Img if img.get("src") and ("/program/" in img.get("src") or "/shared/" in img.get("src"))]
-
-        # データを一つのループで処理
-        def a_result(ttl,img,data):
-            results = []
-            for i in range(len(ttl)):
-                title = ttl[i].get_text()
-                imagine = img[i]
-                overview = data[i].get_text()
-                anime_result = title + imagine + overview
-                results.append(anime_result)
-            return results
-
-        anime_result = a_result(ttl=anime_Ttl, img=anime_Img, data=anime_data)
-
-        line_bot_api.reply_message(ReplyMessageRequest(
-            replyToken=event.reply_token,
-            messages=[TextMessage(text=anime_result)]
-        ))
-
-
+## 起動確認用ウェブサイトのトップページ
+@app.route('/', methods=['GET'])
+def toppage():
+	return 'Hello world!'
 
 ## ボット起動コード
 if __name__ == "__main__":
-    ## ローカルでテストする時のために、`debug=True` にしておく
-    app.run(host="0.0.0.0", port=8000, debug=True)
+	## ローカルでテストする時のために、`debug=True` にしておく
+	app.run(host="0.0.0.0", port=8000, debug=True)
+
