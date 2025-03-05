8ead5a7a5deb526fbff4d97ba34239915fcb762d
osada <sub210sub@gmail.com>
2025-03-05 15:42:28 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import json
-import requests
+import os
 from flask import Flask, request, abort
-from cachetools import TTLCache
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (
     ApiClient, Configuration, MessagingApi,
-    ReplyMessageRequest, TextMessage, FlexMessage
+    ReplyMessageRequest, PushMessageRequest,
+    TextMessage, FlexMessage
 )
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
-import os
 from dotenv import load_dotenv
 
-# .env 読み込み
+# .env ファイル読み込み
 load_dotenv()
 
-# 環境変数を取得
+# 環境変数を変数に割り当て
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
@@ -28,12 +24,17 @@ app = Flask(__name__)
 configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+# コールバックのおまじない
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -43,64 +44,51 @@ def callback():
     return 'OK'
 
 # フレックスメッセージの内容
-flex_content = {
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://eiga.k-img.com/images/anime/program/112402/photo/32b8213a9a89c167/160.jpg?1740713414",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "TITLE",
-        "weight": "bold",
-        "size": "xl"
-      },
-      {
-        "type": "text",
-        "text": "DESCRIPTION",
-        "wrap": True,
-        "size": "sm"
-      }
-    ]
-  },
-  "footer": {  # フッター部分を追加
-    "type": "box",
-    "layout": "horizontal",
-    "contents": [
-      {
-        "type": "button",
-        "style": "primary",
-        "action": {
-          "type": "uri",
-          "label": "Learn more",
-          "uri": "https://example.com"
+def create_flex_message():
+    return FlexMessage(
+        alt_text="シンプルなフレックスメッセージ",
+        contents={
+            "type": "bubble",
+            "hero": {
+                "type": "image",
+                "url": "https://example.com/your-image.jpg",
+                "size": "full",
+                "aspectRatio": "20:13",
+                "aspectMode": "cover"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "タイトル",
+                        "weight": "bold",
+                        "size": "xl"
+                    },
+                    {
+                        "type": "text",
+                        "text": "これはシンプルなフレックスメッセージの説明です。",
+                        "wrap": True,
+                        "size": "sm"
+                    }
+                ]
+            }
         }
-      }
-    ]
-  }
-}
-
+    )
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    # APIインスタンス化
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
 
+    # 受信メッセージの中身を取得
     received_message = event.message.text
 
-    if received_message == "@anime":
-        message = FlexMessage(
-            alt_text="アニメ情報",
-            contents=flex_content  # フレックスメッセージの内容
-        )
-
+    # @test が受信された場合、フレックスメッセージを送信
+    if received_message == "@test":
+        message = create_flex_message()  # フレックスメッセージの作成
         line_bot_api.reply_message(ReplyMessageRequest(
             replyToken=event.reply_token,
             messages=[message]
