8bdae992626b798c68fd54caaaedf274787368aa
osada <sub210sub@gmail.com>
2025-03-07 01:19:38 +0900

    フレックスメッセージで送信するように変更

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def create_anime_flex_message_from_scraping(start_index=0, count=10):
 
     for anime in anime_list[start_index:start_index + count]:
         bubble = FlexBubble(
-            size='mega',  # サイズを小さく設定
+            size='kilo',
             header=FlexBox(
                 layout='vertical',
                 contents=[
